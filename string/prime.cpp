737a500d614d60bb6306ca42f9dc64d9104a0d7b
Kanishka Vijayvargiya <2019076@iiitdmj.ac.in>
2020-10-01 19:47:49 +0530

    A c++ program to check whether the number entered by the user is prime or not

---
+++ string/prime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n,i;
+  bool prime=true;
+  cin>>n;
+  if(n==0 || n==1)
+     prime=false;
+  for(i=2;i<=n/2;i++)
+  {
+     if(n%i==0)
+     {
+        prime=false;
+        break;
+      }
+  }
+  if(prime)
+    cout<<n<<"is a prime number"<<endl;
+  else
+    cout<<n<<"is not a prime number"<<endl;
+  return 0;
+}
